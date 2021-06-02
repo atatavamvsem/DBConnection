26c8293b9f2176b99e817adefc9f2969df2b8f4b
Anton Gubko <atatavamvsembudet@gmail.com>
2021-06-02 15:05:14 +0300

    Add project files.

---
+++ ResultPrinter.cs
@@ -0,0 +1,28 @@
+﻿using log4net;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleDBConnection
+{
+    class ResultPrinter
+    {
+        private static readonly ILog logger = Log4Net.GetInstance();
+        internal static void Print(MySqlDataReader reader)
+        {
+            while (reader.Read())
+            {
+                string outputString = "";
+                for (int f = 0; f < reader.FieldCount; f++)
+                {
+                    outputString = outputString + reader[f] + " ";
+                }
+                logger.Info(outputString);
+            }
+            reader.Close();
+        }
+    }
+}
---
+++ SqlManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace ConsoleDBConnection
+{
+    class SqlManager
+    {
+        static MySqlCommand command;
+        static MySqlConnection conn;
+        static readonly string connStr = "server=" + Properties.ConfData.Server +
+                            ";user=" + Properties.ConfData.User +
+                            ";database=" + Properties.ConfData.DB +
+                            ";password=" + Properties.ConfData.Password + ";";
+        public static void CreateConn() 
+        { 
+            conn = new MySqlConnection(connStr);
+            conn.Open();
+        }
+
+        public static MySqlDataReader ExecuteQuery(string query)
+        {
+            command = new MySqlCommand(query, conn);
+            return command.ExecuteReader();
+        }
+
+        public static void CloseConn()
+        {
+            conn.Close();
+        }
+    }
+}
